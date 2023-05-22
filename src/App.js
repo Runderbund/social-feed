073ab5f09a1a970c059a0eca0800f4f0e328f717
unknown <sean.herbison@gmail.com>
2023-05-22 15:16:54 -0700

    Adjusted header and made the page background blue.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {useState} from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import PostList from './Components/PostList/PostList';
 import CreatePostForm from './Components/CreatePostForm/CreatePostForm';
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className='background'>
       <NavBar />
     </div>
+
   );
 }
 
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const NavBar = () => {
   return (
     <header style={{ backgroundColor: "white" }}>
-        <h2 style={{ margin: "1em" }}>Social
+    {/* Uses in-line styling to make white bar at top of blue page */}
+        <h2 style={{ padding: "1em"}}>Social
         <small className="text-muted" >Feed</small></h2>
     </header>
   );
