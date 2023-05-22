761233ae7900cf45edff569418e2ac672c0253e8
unknown <sean.herbison@gmail.com>
2023-05-22 15:59:58 -0700

    Added Post skeleton.

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+
+const Post = () => {
+    return (
+        <div className="post-form">
+            <b>Name</b>
+            <p>Post content</p>
+            <button>Like</button>
+            <button>Dislike</button>
+            <hr />
+        </div>
+    );    
+};
+export default Post;
