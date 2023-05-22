4d1fd3fa46613deadcda371ab1bff8a8d4f634a1
unknown <sean.herbison@gmail.com>
2023-05-22 16:46:37 -0700

    Added handleSubmit function to App.js.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, {useState} from 'react';
-import NavBar from './Components/NavBar/NavBar';
-import PostList from './Components/PostList/PostList';
-import CreatePostForm from './Components/CreatePostForm/CreatePostForm';
-import './App.css';
+import React, { useState } from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import PostList from "./Components/PostList/PostList";
+import CreatePostForm from "./Components/CreatePostForm/CreatePostForm";
+import "./App.css";
 
 function App() {
   const [posts, setPosts] = useState([]);
 
+  const handleSubmit = (name, postText) => {
+    const date = new Date();
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const post = {
+      name,
+      postText,
+      date: `${date.getDate()} ${
+        monthNames[date.getMonth()]
+      } ${date.getFullYear()}`,
+      // Displays as e.g., 2 June 2021
+      time: `${date.getHours()}:${date.getMinutes()}`,
+      // Dsiplays 24-hour time. May add logic later to handle AM/PM
+      liked: false,
+      disliked: false,
+    };
+    setPosts([post, ...posts]);
+  };
 
   return (
-    <div className='background'>
+    <div className="background">
       <NavBar />
-      <CreatePostForm />
+      <CreatePostForm handleSubmit={handleSubmit} />
       {/* <PostList posts={posts}/> */}
       {/* posts not defined yet */}
     </div>
-
   );
 }
 
